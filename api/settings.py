2dbee06d2765b027049fc6140403047cb74c8517
Lais Gabrielle Lodi <laislodi@yahoo.com.br>
2021-06-17 18:48:04 -0700

    Configure CORS - Allowing the frontend project to do requests

--- api/settings.py
+++ api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'graphene_django',
     "api",
     'django.contrib.admin',
@@ -46,6 +47,7 @@ GRAPHENE = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +55,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    # "http://127.0.0.1:3000"
 ]
 
 ROOT_URLCONF = 'api.urls'
